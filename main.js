3c830c2d4ca2d20fc3729d3404e2ff1fb586479c
Michael Chung <mung.chung2257@gmail.com>
2026-07-14 21:13:11 -0400

    feat: save and delete brains

--- main.js
+++ main.js
@@ -10,6 +10,10 @@ const road = new Road(carCanvas.width/2,carCanvas.width*0.9);
 
 const N=100;
 const cars = generateCars(N);
+let bestCar=cars[0];
+if(localStorage.getItem("bestBrain")){
+    bestCar.brain=JSON.parse(localStorage.getItem("bestBrain"));
+}
 
 
 const traffic = [
@@ -17,6 +21,15 @@ const traffic = [
 ];
 
 animate();
+
+function save(){
+    localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
+
+}
+
+function discard(){
+    localStorage.removeItem("bestBrain");
+}
 
 function generateCars(N){
     const cars=[];
@@ -35,7 +48,7 @@ function animate(time){
         cars[i].update(road.borders,traffic);
     }
 
-    const bestCar=cars.find(
+    bestCar=cars.find(
         c=>c.y==Math.min(
             ...cars.map(c=>c.y)
         )
